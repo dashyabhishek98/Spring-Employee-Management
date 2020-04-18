fc40713cbe68c1d0a39b20b2011635547ef4e846
dashy <dashy@DESKTOP-HSQ84PL>
2020-04-18 23:37:42 +0530

    Employee Management using Spring Data Rest.

--- EmployeeManagement/src/main/java/com/example/employee/EmployeeManagementApplication.java
+++ EmployeeManagement/src/main/java/com/example/employee/EmployeeManagementApplication.java
@@ -2,9 +2,6 @@ package com.example.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import com.example.employee.rest.EmployeeRestController;
 
 @SpringBootApplication
 public class EmployeeManagementApplication {
